39fbcccc7a76e0db847a55afcd8ec81c6b5f34ea
dingeHCHS <69469881+dingeHCHS@users.noreply.github.com>
2021-01-19 11:43:32 -0600

    Add files via upload

---
+++ data.js
@@ -0,0 +1,450 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-img_20201112_112221_00_031",
+      "name": "IMG_20201112_112221_00_031",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -0.03222146311374274,
+        "pitch": 0.23262893603874701,
+        "fov": 1.3715802068843215
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.02653087554933542,
+          "pitch": 0.3321870902288957,
+          "rotation": 0,
+          "target": "1-img_20201112_120503_00_035"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.388381521804412,
+          "pitch": -0.32104282098607406,
+          "title": "Welcome to the CTE Unit",
+          "text": '<iframe src="https://player.vimeo.com/video/31386280" width="640" height="360" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>'
+        }
+      ]
+    },
+    {
+      "id": "1-img_20201112_120503_00_035",
+      "name": "IMG_20201112_120503_00_035",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -3.1148003215914315,
+        "pitch": 0.20015034287930789,
+        "fov": 1.3715802068843215
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.1409680694428133,
+          "pitch": 0.4311931805622642,
+          "rotation": 0,
+          "target": "2-img_20201112_120652_00_036"
+        },
+        {
+          "yaw": 0.006213279688122242,
+          "pitch": 0.48693300150149454,
+          "rotation": 0,
+          "target": "0-img_20201112_112221_00_031"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.363342639196606,
+          "pitch": -0.15097508316994634,
+          "title": "Launch",
+          "text": '<iframe src="https://drive.google.com/file/d/1OYS4YehEjzhtaJKiqysGnCcU_hs2QRFK/preview" width="640" height="480"></iframe>'
+        }
+      ]
+    },
+    {
+      "id": "2-img_20201112_120652_00_036",
+      "name": "IMG_20201112_120652_00_036",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -3.101631961654448,
+        "pitch": 0.20093047366833794,
+        "fov": 1.3715802068843215
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.1040178838750574,
+          "pitch": 0.44259608022377783,
+          "rotation": 0,
+          "target": "3-img_20201112_120826_00_037"
+        },
+        {
+          "yaw": 0.012889858124317755,
+          "pitch": 0.4881855221131062,
+          "rotation": 0,
+          "target": "1-img_20201112_120503_00_035"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.3111742127494495,
+          "pitch": -0.29326325886645144,
+          "title": "Visual Communications",
+          "text": '<iframe width="560" height="315" src="https://www.youtube.com/embed/mkHyRbT7wtI" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+        }
+      ]
+    },
+    {
+      "id": "3-img_20201112_120826_00_037",
+      "name": "IMG_20201112_120826_00_037",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 3.1228510148974706,
+        "pitch": 0.27287140783098707,
+        "fov": 1.3715802068843215
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.1302811073861943,
+          "pitch": 0.5145261537563819,
+          "rotation": 0,
+          "target": "4-img_20201112_120919_00_038"
+        },
+        {
+          "yaw": -0.034098088239607804,
+          "pitch": 0.5272457686331791,
+          "rotation": 0,
+          "target": "2-img_20201112_120652_00_036"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.9357682681844359,
+          "pitch": -0.2510904192822103,
+          "title": "Tech and Fincance",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "4-img_20201112_120919_00_038",
+      "name": "IMG_20201112_120919_00_038",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -3.092874609908188,
+        "pitch": 0.24780834291922105,
+        "fov": 1.3715802068843215
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.0829002447551996,
+          "pitch": 0.5592876721324771,
+          "rotation": 0,
+          "target": "5-img_20201112_112431_00_033"
+        },
+        {
+          "yaw": 0.031869538734168756,
+          "pitch": 0.608190575656014,
+          "rotation": 0,
+          "target": "3-img_20201112_120826_00_037"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.9776300460507112,
+          "pitch": -0.3144302356647568,
+          "title": "Marketing",
+          "text": '<iframe width="560" height="315" src="https://www.youtube.com/embed/ndrT93TqZoU" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+        },
+        {
+          "yaw": 1.7284379218872328,
+          "pitch": -0.3029929908531752,
+          "title": "Pre-Engineering",
+          "text": '<iframe width="560" height="315" src="https://www.youtube.com/embed/8oFZxu31Ih0" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+        }
+      ]
+    },
+    {
+      "id": "5-img_20201112_112431_00_033",
+      "name": "IMG_20201112_112431_00_033",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 0.020138414446089214,
+        "pitch": 0.2717105972932501,
+        "fov": 1.3715802068843215
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.03763673532382583,
+          "pitch": 0.5444579829665912,
+          "rotation": 0,
+          "target": "6-img_20201112_121021_00_039"
+        },
+        {
+          "yaw": -3.097209342589725,
+          "pitch": 0.611894403633114,
+          "rotation": 0,
+          "target": "4-img_20201112_120919_00_038"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-img_20201112_121021_00_039",
+      "name": "IMG_20201112_121021_00_039",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -3.092380597268779,
+        "pitch": 0.26756928439178296,
+        "fov": 1.3715802068843215
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.0471356988344382,
+          "pitch": 0.5664841887584728,
+          "rotation": 0,
+          "target": "7-img_20201112_112520_00_034"
+        },
+        {
+          "yaw": 0.0007229052368273869,
+          "pitch": 0.6607230617994198,
+          "rotation": 0,
+          "target": "5-img_20201112_112431_00_033"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.6344012514237765,
+          "pitch": -0.34290031822729716,
+          "title": "Information Technology",
+          "text": '<iframe width="560" height="315" src="https://www.youtube.com/embed/dqrDAQ3kZgo" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+        }
+      ]
+    },
+    {
+      "id": "7-img_20201112_112520_00_034",
+      "name": "IMG_20201112_112520_00_034",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -0.04296195081832366,
+        "pitch": 0.26054440836339054,
+        "fov": 1.3715802068843215
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.01552851460647986,
+          "pitch": 0.5475520507052885,
+          "rotation": 0,
+          "target": "8-img_20201112_121202_00_041"
+        },
+        {
+          "yaw": 3.1405625707762077,
+          "pitch": 0.5140157082158829,
+          "rotation": 0,
+          "target": "6-img_20201112_121021_00_039"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-img_20201112_121202_00_041",
+      "name": "IMG_20201112_121202_00_041",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -3.082716636993368,
+        "pitch": 0.27672341761638464,
+        "fov": 1.3715802068843215
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.006742904207463596,
+          "pitch": 0.4006188204012062,
+          "rotation": 0,
+          "target": "7-img_20201112_112520_00_034"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.840935938297335,
+          "pitch": -0.35337611599730856,
+          "title": "Agriculture",
+          "text": '<iframe width="560" height="315" src="https://www.youtube.com/embed/DIF-IZjsJwo" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
